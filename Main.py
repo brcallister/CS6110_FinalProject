0bd57be0a8744267789a5c440e2d0aef3523c8de
Brian Callister <55854515+brcallister@users.noreply.github.com>
2024-04-07 19:53:16 -0600

    General Updates

--- Main.py
+++ Main.py
@@ -9,17 +9,17 @@ from src.Simulation import Simulation
 from src.OutputData import draw_image
 
 if __name__ == "__main__":
-    # Read layout information in from file
-    rawLayout = read_in_data(os.path.join("input", "Debug1.txt"))
+    # Read layout information in from file and prep output dir
+    numRows, numCols, rawLayout = read_in_data("Debug1.txt")
 
     # Create full environment according to specs
-    env = Environment(rawLayout, numAgents=10)
+    env = Environment(numRows, numCols, rawLayout, numAgents=10)
 
     # Specify the type of simulation to run
-    simulation = Simulation(numSteps=15, printFreq=5, graphicFreq=5, outputInitial=True)
+    simulation = Simulation(numSteps=10, printFreq=10, graphicFreq=5, outputInitial=True)
 
     # Run the actual simulation
     finalEnv = simulation.runSimulation(env)
 
     # Output the final world state
-    draw_image(finalEnv, 'TODO_FinalDebug.png')
+    draw_image(finalEnv, 'FinalMap.png')
--- src/OutputData.py
+++ src/OutputData.py
@@ -3,15 +3,61 @@
 # =======================
 
 import os
+import matplotlib.pyplot as plt
+
+OUTPUT_DIR = 'output'
+if not os.path.exists('output'):
+    os.makedirs('output')
 
 def draw_image(environment, filename):
-    print('TODO: draw a pretty picture, output to filename')
-    pass
+    ax = plt.gca()
+
+    # Loop through (potentially jagged) array
+    for row_index, row in enumerate(environment.map[::-1]):
+        for col_index, location in enumerate(row):
+            for thing in location.thingsHere:
+                color = 'white'
+                marker = 'o'
+                if thing.type == 'WALL':
+                    color = 'black'
+                    marker = 's'
+                elif thing.type == 'EXIT':
+                    color = 'blue'
+                    marker = 'o'
+                elif thing.type == 'TODO_Agent':
+                    color = 'red'
+                    marker = 'x'
+                # Plot a point at current location with determined color
+                ax.plot(col_index, row_index, marker=marker, color=color)
+
+    ax.set_title(filename, fontsize=10, fontweight='bold')
+    plt.gca().set_yticks(ax.get_yticks())
+    plt.gca().set_yticklabels(ax.get_yticklabels()[::-1])
+
+    # Show plot
+    # plt.show()
+
+    plt.savefig(os.path.join(OUTPUT_DIR, filename))
 
 def print_env_to_console(environment):
-    print('TODO: pretty print environment state to console')
-    pass
+    print('Current Enviroment:')
+    # Loop through (potentially jagged) array
+    for row in environment.map:
+        rowBuffer = ''
+        for location in row:
+            if location.isEntityHere('WALL'):
+                rowBuffer += 'X'
+            elif location.isEntityHere('EXIT'):
+                rowBuffer += 'O'
+            else:
+                rowBuffer += ' '
+        print(rowBuffer)
+    print()
 
 def print_stats_to_console(environment):
-    print('TODO: maybe print additional stats to console?')
+    # TODO: maybe print additional stats to console?
+    # print('Current Stats: ')
+    # print('Agents safe: ')
+    # print('Agents still in building: ')
+    # print('Time taken: ')
     pass
