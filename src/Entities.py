8addc966e39bcfc9a1d111d0175d2e260117129c
Joel Gilbert <joelgilbert1618@gmail.com>
2024-04-17 19:36:33 -0600

    function stubs and members of Agent class

--- src/Entities.py
+++ src/Entities.py
@@ -10,4 +10,19 @@ class Agent(Entity):
     def __init__(self, entityType, id):
         super().__init__(entityType)
         self.id = id
-        # TODO we should have all data that the agent needs to track in here, such as type or instance info
+        self.currentLocation = []      # [x,y]
+        self.desiredLocation = []      # [x,y] of the location they want to move to
+        self.familiarityWithExit = 10  # TODO: figure out how this actually works
+        self.numberTimesNotMoved = 0
+
+    def pickDesiredLocation(self, environment):
+        # based off of the environment
+        # can only move to a currently empty location
+        # want to move towards the exit
+        pass
+
+    def changeRoles(self):
+        # if they haven't moved after a certain number of times, switch from
+        # cooperate to betray
+        # use the formulas in the paper
+        pass
