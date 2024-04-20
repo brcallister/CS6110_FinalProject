6bd449bbef33778e4ff45bd7322b4695912d5109
David Storey <david.storey@aggiemail.usu.edu>
2024-04-19 23:00:42 -0600

    started with some maths

--- src/Entities.py
+++ src/Entities.py
@@ -2,27 +2,48 @@
 # CS 6110 - Final Project
 # =======================
 
+import math
+
 class Entity:
-    def __init__(self, entityType):
-        self.type = entityType
+	def __init__(self, entityType):
+		self.type = entityType
 
 class Agent(Entity):
-    def __init__(self, entityType, id):
-        super().__init__(entityType)
-        self.id = id
-        self.currentLocation = []      # [x,y]
-        self.desiredLocation = []      # [x,y] of the location they want to move to
-        self.familiarityWithExit = 10  # TODO: figure out how this actually works
-        self.numberTimesNotMoved = 0
+	def __init__(self, entityType, id):
+		super().__init__(entityType)
+		self.id = id
+		self.currentLocation = []      # [x,y]
+		self.desiredLocation = []      # [x,y] of the location they want to move to
+		self.familiarityWithExit = 10  # TODO: figure out how this actually works
+		self.numberTimesNotMoved = 0
 
-    def pickDesiredLocation(self, environment):
-        # based off of the environment
-        # can only move to a currently empty location
-        # want to move towards the exit
-        pass
+	def calculateSF(self, location, exit):
+		sf = 1/(math.sqrt((exit[0] - location[0])**2 + (exit[1] - location[1])**2))
+		return sf
 
-    def changeRoles(self):
-        # if they haven't moved after a certain number of times, switch from
-        # cooperate to betray
-        # use the formulas in the paper
-        pass
+	def pickDesiredLocation(self, potentialMoves, exits):
+		if len(potentialMoves) == 0:
+			return
+		movementProbs = {}
+		# For every exit that exists, we need to find what exit the agent will move toward
+		for exit in exits:
+			# Find the sum of all SF values for this potential move (3 figure 2 in paper)
+			sumSf = 0
+			for move in potentialMoves:
+				sumSf += math.exp(self.familiarityWithExit * self.calculateSF(move, exit))
+			# Now calculate the probability of making that move (3 figure 2 in paper)
+			for move in potentialMoves:
+				moveSf = self.calculateSF(move, exit)
+				prob = math.exp(self.familiarityWithExit*moveSf) / sumSf
+				# using a tuple as a key will allow us to konw what move towards what exit is best for our agent
+				movementProbs[(move[0], move[1], exit[0], exit[1])] = prob
+		
+		# Now that we have all probabilites, we need to find the best one.
+		bestMove = max(movementProbs, key=movementProbs.keys)
+		return bestMove
+
+	def changeRoles(self):
+		# if they haven't moved after a certain number of times, switch from
+		# cooperate to betray
+		# use the formulas in the paper
+		pass
--- src/Environment.py
+++ src/Environment.py
@@ -3,55 +3,84 @@
 # =======================
 
 import random
+import copy
 
 from src.Entities import Entity
+from src.Entities import Agent
 
 class Environment:    
-    def __init__(self, numRows, numCols, rawLayout):
-        self.numRows = numRows
-        self.numCols = numCols
-        fullMap = []
+	def __init__(self, numRows, numCols, rawLayout):
+		self.numRows = numRows
+		self.numCols = numCols
+		fullMap = []
+		exits = []
 
-        # Set up map layout
-        for line in rawLayout:
-            row = []
-            for char in line:
-                # Defaults to empty location
-                newLocation = self.Location([])
-                # Wall
-                if char == 'X':
-                    newLocation = self.Location([Entity('WALL')])
-                # Exit
-                elif char == 'O':
-                    newLocation = self.Location([Entity('EXIT')])
-                # Agent B
-                elif char == 'b':
-                    newLocation = self.Location([Entity('AgentBetray')])
-                # Agent C
-                elif char == 'c':
-                    newLocation = self.Location([Entity('AgentCooperate')])
-                # TODO - add additional items here
-                row.append(newLocation)
-            while len(row) < numRows:
-                row.append(self.Location([]))
-            fullMap.append(row)
-        while len(fullMap) < numCols:
-            fullMap.append([self.Location([]) for _ in range(numRows)])
+		# Set up map layout
+		for i in range(len(rawLayout)):
+			row = []
+			for j in range(len(rawLayout[i])):
+				# Defaults to empty location
+				newLocation = self.Location([])
+				# Wall
+				if rawLayout[i][j] == 'X':
+					newLocation = self.Location([Entity('WALL')])
+				# Exit
+				elif rawLayout[i][j] == 'O':
+					newLocation = self.Location([Entity('EXIT')])
+					exits.append([i, j])
+				# Agent B
+				elif rawLayout[i][j] == 'b':
+					newLocation = self.Location([Entity('AgentBetray')])
+				# Agent C
+				elif rawLayout[i][j] == 'c':
+					newLocation = self.Location([Entity('AgentCooperate')])
+				# TODO - add additional items here
+				row.append(newLocation)
+			while len(row) < numRows:
+				row.append(self.Location([]))
+			fullMap.append(row)
+		while len(fullMap) < numCols:
+			fullMap.append([self.Location([]) for _ in range(numRows)])
 
-        self.map = fullMap
-    
-    def runOneTimeStep(self):
-        # TODO give all agents a chance to move
-        pass
+		self.map = fullMap
+		self.exits = exits
+	
+	def findPotentialMoves(self, position):
+		maxRows = len(self.map)
+		maxCols = len(self.map[0])
+		agentX = position[0]
+		agentY = position[1]
+		potentialMoves = []
+		# Find all suitable spaces an Agent can move to
+		for i in range(max(agentX-1, 0), min(agentX+2, maxRows)):
+			for j in range(max(agentY-1, 0), min(agentY+2, maxCols)):
+				if i == agentX and j == agentY:
+					continue
+				potentialLocation = self.Location(self.map[i][j])
+				# If there is not another agent or a wall, that location is available to move to.
+				if not potentialLocation.isEntityHere([[Entity('AgentBetray')], [Entity('AgentCooperate')], [Entity('WALL')]]):
+					potentialMoves.append(self.map[i][j])
+		
+		return potentialMoves
+	
+	def runOneTimeStep(self, agents):
+		potentialMap = copy.deepcopy(self.map)
+		for agent in agents:
+			# Find current empty spaces
+			agentPosition = agent.currentLocation
+			potentialMoves = self.findPotentialMoves(agentPosition)
+			agentPickedMove = agent.pickDesiredLocation(potentialMap, self.exits)
+		# TODO give all agents a chance to move
+		pass
 
-    class Location:
-        def __init__(self, entitiesHere):
-            self.thingsHere = entitiesHere
+	class Location:
+		def __init__(self, entitiesHere):
+			self.thingsHere = entitiesHere
 
-        def isEntityHere(self, thingToFind):
-            for thing in self.thingsHere:
-                if thing.type == thingToFind:
-                    return True
-            return False
+		def isEntityHere(self, thingToFind):
+			for thing in self.thingsHere:
+				if thing.type == thingToFind:
+					return True
+			return False
 
 
